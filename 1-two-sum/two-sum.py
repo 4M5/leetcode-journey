bc155d9616690e79cccdcdc819d738e3d65bfc22
Adil <84286609+4M5@users.noreply.github.com>
2026-08-05 22:32:52 +0530

    Time: 1 ms (61.64%) | Memory: 13.1 MB (52.89%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        hashmap = {}
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            if diff not in hashmap:
+                hashmap[nums[i]] = i
+            else:
+                return [hashmap[diff],i]
+
